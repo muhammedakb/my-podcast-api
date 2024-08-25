2626661d8250384d6f119125229702abe1c95480
Muhammet Akbulut <muhammetakb68@gmail.com>
2024-08-25 14:33:50 +0300

    user login & generate token

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EpisodesModule } from './episodes/episodes.module';
 import { TopicsModule } from './topics/topics.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EpisodesModule, TopicsModule],
+  imports: [EpisodesModule, TopicsModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/jwt-secret.ts
@@ -0,0 +1,4 @@
+// FIXME: change this one
+// TODO: don't use this secret for production
+export const JWT_SECRET =
+  "if-at-first-you-don't-succeed-dust-yourself-off-and-try-again";
